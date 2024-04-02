8cd4d349b9e49f264bf6dad8d439362dc0d49ce2
minsion <15238290258@163.com>
2024-04-02 14:05:30 +0800

    modify design and modify readme information

--- README.md
+++ README.md
@@ -9,5 +9,11 @@ yarn start
 
 Runs the app in the development mode Open [http://localhost:8081](http://localhost:8081) to view it in your browser.
 
+## designer page
 
 <img src="./public/1.png" />
+
+## preview page
+
+<img src="./public/2.png" />
+
--- src/pages/designer/index.js
+++ src/pages/designer/index.js
@@ -82,7 +82,6 @@ function DataProvider(props) {
     props.dispatch({ type: "component/mode", data: "development" });
     props.dispatch({ type: "component/querys", data: pathToParam() });
     loadScript("https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css", "css");
-    fetchData();
     storageData();
   }, []);
 
